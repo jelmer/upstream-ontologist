c5cd967e8ed2027054071e546a890b639b166ad1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-01-30 16:38:38 +0000

    Fix rules import.

--- upstream_ontologist/guess.py
+++ upstream_ontologist/guess.py
@@ -224,7 +224,7 @@ def update_from_guesses(upstream_metadata, guessed_items):
 
 
 def guess_from_debian_rules(path, trust_package):
-    from lintian_brush.rules import Makefile
+    from debmutate._rules import Makefile
     mf = Makefile.from_path(path)
     try:
         upstream_git = mf.get_variable(b'UPSTREAM_GIT')
