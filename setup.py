211be674f367613c505d823ecad25dd22e14f050
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-02-05 00:18:55 +0000

    Release 0.1.7.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         "upstream_ontologist.debian",
         "upstream_ontologist.tests",
     ],
-    version="0.1.6",
+    version="0.1.7",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     url="https://github.com/jelmer/upstream-ontologist",
