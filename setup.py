0feb51feb6f9cbfd8db809f948a4be193fa58d9e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-16 12:24:55 +0000

    Release 0.1.14.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         "upstream_ontologist.debian",
         "upstream_ontologist.tests",
     ],
-    version="0.1.13",
+    version="0.1.14",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     url="https://github.com/jelmer/upstream-ontologist",
--- upstream_ontologist/__init__.py
+++ upstream_ontologist/__init__.py
@@ -55,7 +55,7 @@ from typing import Optional, Sequence
 
 SUPPORTED_CERTAINTIES = ["certain", "confident", "likely", "possible", None]
 
-version_string = "0.1.13"
+version_string = "0.1.14"
 
 USER_AGENT = "upstream-ontologist/" + version_string
 # Too aggressive?
