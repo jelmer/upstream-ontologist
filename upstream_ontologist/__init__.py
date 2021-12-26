a9d883a8e8f0ba9aac1307b7ac82336964e43a42
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-12-26 15:08:22 +0000

    Release 0.1.24.

--- upstream_ontologist/__init__.py
+++ upstream_ontologist/__init__.py
@@ -60,7 +60,7 @@ from email.utils import parseaddr
 
 SUPPORTED_CERTAINTIES = ["certain", "confident", "likely", "possible", None]
 
-version_string = "0.1.23"
+version_string = "0.1.24"
 
 USER_AGENT = "upstream-ontologist/" + version_string
 # Too aggressive?
