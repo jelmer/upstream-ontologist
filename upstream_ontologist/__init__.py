7d31d2c7aba2b9d39ba5176d404f1422a71647c1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-01-27 00:30:38 +0000

    Release 0.1.33.

--- upstream_ontologist/__init__.py
+++ upstream_ontologist/__init__.py
@@ -62,7 +62,7 @@ from urllib.parse import urlparse
 
 SUPPORTED_CERTAINTIES = ["certain", "confident", "likely", "possible", None]
 
-version_string = "0.1.32"
+version_string = "0.1.33"
 
 USER_AGENT = "upstream-ontologist/" + version_string
 # Too aggressive?
