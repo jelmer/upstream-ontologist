d8d790bb2cfbe3cafc84d60cb1390d4912b4528e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-05-23 18:35:55 +0100

    Add basic rust interface

---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+use pyo3::prelude::*;
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+pub struct Person {
+    pub name: Option<String>,
+    pub email: Option<String>,
+    pub url: Option<String>,
+}
+
+impl FromPyObject<'_> for Person {
+    fn extract(ob: &'_ PyAny) -> PyResult<Self> {
+        let name = ob.getattr("name")?.extract::<Option<String>>()?;
+        let email = ob.getattr("email")?.extract::<Option<String>>()?;
+        let url = ob.getattr("url")?.extract::<Option<String>>()?;
+        Ok(Person { name, email, url })
+    }
+}
+
+pub enum UpstreamDatum {
+    Name(String),
+    Homepage(String),
+    Repository(String),
+    RepositoryBrowse(String),
+    Description(String),
+    Summary(String),
+    License(String),
+    Author(Vec<Person>),
+    BugDatabase(String),
+    BugSubmit(String),
+    Contact(String),
+    CargoCrate(String),
+    SecurityMD(String),
+    SecurityContact(String),
+    Version(String),
+}
+
+pub fn guess_upstream_metadata(
+    path: PathBuf,
+    trust_package: Option<bool>,
+    net_access: Option<bool>,
+    consult_external_directory: Option<bool>,
+    check: Option<bool>,
+) -> Result<Vec<UpstreamDatum>, ()> {
+    Python::with_gil(|py| {
+        let guess = py.import("upstream_ontologist.guess").unwrap();
+        let guess_upstream_metadata = guess.getattr("guess_upstream_metadata").unwrap();
+
+        let items = guess_upstream_metadata
+            .call1((
+                path,
+                trust_package,
+                net_access,
+                consult_external_directory,
+                check,
+            ))
+            .unwrap()
+            .extract::<HashMap<String, PyObject>>()
+            .unwrap();
+
+        let mut ret = Vec::new();
+        for (name, value) in items.into_iter() {
+            if value.is_none(py) {
+                continue;
+            }
+            let entry = match name.as_str() {
+                "Homepage" => UpstreamDatum::Homepage(value.extract::<String>(py).unwrap()),
+                "Name" => UpstreamDatum::Name(value.extract::<String>(py).unwrap()),
+                "Repository" => UpstreamDatum::Repository(value.extract::<String>(py).unwrap()),
+                "Repository-Browse" => {
+                    UpstreamDatum::RepositoryBrowse(value.extract::<String>(py).unwrap())
+                }
+                "Bug-Database" => UpstreamDatum::BugDatabase(value.extract::<String>(py).unwrap()),
+                "Bug-Submit" => UpstreamDatum::BugSubmit(value.extract::<String>(py).unwrap()),
+                "Contact" => UpstreamDatum::Contact(value.extract::<String>(py).unwrap()),
+                "X-Security-MD" => UpstreamDatum::SecurityMD(value.extract::<String>(py).unwrap()),
+                "Security-Contact" => {
+                    UpstreamDatum::SecurityContact(value.extract::<String>(py).unwrap())
+                }
+                "X-Cargo-Crate" => UpstreamDatum::CargoCrate(value.extract::<String>(py).unwrap()),
+                "X-Description" => UpstreamDatum::Description(value.extract::<String>(py).unwrap()),
+                "X-Summary" => UpstreamDatum::Summary(value.extract::<String>(py).unwrap()),
+                "X-License" => UpstreamDatum::License(value.extract::<String>(py).unwrap()),
+                "X-Version" => UpstreamDatum::Version(value.extract::<String>(py).unwrap()),
+                "X-Author" => UpstreamDatum::Author(
+                    value
+                        .extract::<Vec<Person>>(py)
+                        .unwrap()
+                        .into_iter()
+                        .collect(),
+                ),
+                _ => {
+                    panic!("{}: {:?}", name, value);
+                }
+            };
+            ret.push(entry);
+        }
+        Ok(ret)
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_guess_upstream_metadata() {
+        guess_upstream_metadata(
+            PathBuf::from("."),
+            Some(true),
+            Some(true),
+            Some(true),
+            Some(true),
+        )
+        .unwrap();
+    }
+}
