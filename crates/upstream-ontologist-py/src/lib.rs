5ce57e1c84da603fd5167157df535d49148fbf79
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-05-30 20:59:10 +0100

    Convert fossil command function to rust

--- crates/upstream-ontologist-py/src/lib.rs
+++ crates/upstream-ontologist-py/src/lib.rs
@@ -5,8 +5,14 @@ fn url_from_git_clone_command(command: &[u8]) -> Option<String> {
     upstream_ontologist::url_from_git_clone_command(command)
 }
 
+#[pyfunction]
+fn url_from_fossil_clone_command(command: &[u8]) -> Option<String> {
+    upstream_ontologist::url_from_fossil_clone_command(command)
+}
+
 #[pymodule]
 fn _upstream_ontologist(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(url_from_git_clone_command))?;
+    m.add_wrapped(wrap_pyfunction!(url_from_fossil_clone_command))?;
     Ok(())
 }
